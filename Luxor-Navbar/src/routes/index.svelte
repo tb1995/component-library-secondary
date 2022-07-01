5b7e744d4708ccb820f3fd2afc771498ed7235ae
iamrabie <kishmalakamran359@gmail.com>
2022-07-01 14:28:51 +0500

    Luxor Navbar styling and responsiveness

--- Luxor-Navbar/src/routes/index.svelte
+++ Luxor-Navbar/src/routes/index.svelte
@@ -1,60 +1,87 @@
 <script>
 
     import '../styles/global.css';
+    //import Logo from '/yo.svg';
     //vanila js
     /* window.onmousemove(e) = function()
     {
         console.log(e.pageY);
     } */
     
+  /**
+* @type {number}
+*/
   let y;
+  let display = false;
+  /**
+* @param {{ pageY: number; }} e
+*/
   function OnMouseMove(e)
   {
     y = e.pageY;
     console.log(y);
   }
- 
-    
+
 </script>
 <svelte:window on:mousemove={OnMouseMove}/>
 
-<nav>
-<ul>
+<div class="div-flex">
+<nav class={ y > 90 ? 'nav1' : 'nav'}>
+<img src="src/Luxor-Logo.png" alt="not loading" class="image"/>
+<ul class="{display ? 'ul-block': 'ul-none'}">
     <li>
-        <a href="#">Home</a>
+        <a href="www.facebook.com">Home</a>
     </li>
 
     <li>
-        <a href="">About</a>
+        <a href="www.facebook.com">About</a>
+    </li>
+
+    <li id="img">
+        <a href="www.facebook.com" class="logo Logo2"><img src="src/Luxor-Logo.png" alt="not loading" id="img-li"/></a>
     </li>
 
     <li>
-        <a class="logo"><img src="http://i.imgur.com/WwCbbpG.jpg" alt="luxor"></a>
+        <a href="www.facebook.com">Contact</a>
     </li>
 
     <li>
-        <a href="">Contact</a>
-    </li>
-
-    <li>
-        <a href="">Gallery</a>
+        <a href="www.facebook.com">Gallery</a>
     </li>
 </ul>
-</nav>
-<p>you have scrolled {y} pixels</p>
 
-{#if y > 85}
-console.log('sdf');
-{/if}
+<button>Contact Us</button>
+<div class="Divv" on:click="{ () => display = !display }">
+    <hr>
+    <hr>
+    <hr>
+</div>
+
+
+
+</nav>
+</div>
+
 
 
 <style>
     nav
     {
-        background-color:cornflowerblue;
+        background-color:transparent;
         width: 100%;
         position:sticky;
-        height:10vh;
+        height:16vh;
+        display:flex;
+        
+    }
+
+    .nav1
+    {
+        background-color:navy;
+        width: 100%;
+        position:sticky;
+        height:16vh;
+        border-bottom: white 2px solid;
         
     }
 
@@ -62,33 +89,317 @@ console.log('sdf');
     {
         
         list-style-type: none;
+        width:50%;
         height:10vh;
         margin-top: 0px;
+        margin-left: auto;
+        margin-right: auto;
         text-align: center;
       
     } 
+
 
     nav ul li
     {
         display: inline-block;
         height:6vh;
-        width:115px;
+        width:130px;
+        margin-top: 15px;
     }
+    
 
     nav ul li a
     {
        text-decoration:none;
-       color:black;
+       color:white;
        font-size: 18px;
+       font-weight: bold;
     }
 
     .logo img {
-    margin-top:0px;
+    margin-top:17px;
     display: block;
-    width:100px;
-    height:10vh;
+    width:145px;
+    height:6vh;
     object-fit:cover;
     margin-left: auto;
     margin-right: auto;
     }
+
+    button
+    {
+        background-color: transparent;
+        width:140px;
+        font-size: 16px;
+        margin-right: 2%;
+        height:7vh;
+        margin-top:30px;
+        border:white 2px solid;
+        color:white;
+    }
+    
+    hr{
+        width: 35px;
+        height: 5px;
+        background-color: black;
+        margin: 6px 0;
+    }
+
+    .Divv
+    {
+        display: none;
+    }
+
+    .image
+    {
+        display: none;
+    }
+
+    #img-li
+    {
+        margin-top: 12px;
+    }
+
+  
+    @media screen and (min-width:150px) and (max-width: 529px){
+
+    nav
+    {
+        height: 11vh;
+    }
+    nav ul
+    {
+        display:none;
+    
+    }
+
+    button
+    {
+        display:none;
+    }
+
+    .Divv
+    {
+        display:block;
+        margin-top: 45px;
+        position:absolute;
+        left:80%;
+        cursor: pointer;
+    }
+
+
+    hr{
+        width: 18px;
+        height: 1px;
+        background-color: white;
+        border:white 1px solid;
+        margin: 2px 0;
+        border-radius: 2px;
+    }
+
+    .ul-block
+    {
+        display: block;
+        margin-top:83px;
+        width:100%;
+        height:35vh;
+        background-color: white;
+        padding-inline-start: 0px;
+    }
+
+    .ul-none
+    {
+        display:none;
+    }
+
+    nav ul li
+    {
+        width:100%;
+        padding-top: 20px;
+        margin-top: 0px;
+
+    }
+
+    nav ul li:hover
+    {
+        background-color:#525252;
+        transition: 0.5s;
+    }
+
+    nav ul li a
+    {
+        font-size: 12px;
+        color:black;
+    }
+
+    #img
+    {
+        display: none;
+
+    }
+
+    .nav1
+    {
+        background-color:transparent;
+        width: 100%;
+        position:sticky;
+        height:11vh;
+        border-bottom: 0px;
+    }
+
+    .image
+    {
+        display: block;
+        width: 160px;
+        position: absolute;
+        left:5%;
+        top:28%;
+        height: 7vh;
+    }
+
+
+
+   
+}
+
+@media screen and (min-width:529px) and (max-width: 750px)
+{
+
+    button
+    {
+        display:none;
+    }
+
+    nav ul li a
+    {
+        font-size:14px;
+    }
+
+    nav ul
+    {
+        padding-inline-start:0px;
+        width:100%;
+        margin-block-end:0px;
+        left:0%;
+
+    }
+
+    nav ul li
+    {
+        height:6vh;
+        width:90px;
+    }
+
+
+
+    #img-li
+    {
+        width:120px;
+        height:5vh;
+        margin-top: 16px;
+    }
+
+    #img
+    {
+        width:140px;
+    }
+
+}
+
+@media screen and (min-width:751px) and (max-width: 1000px)
+{
+
+    button
+    {
+        display:block;
+        margin-top: 30px;
+
+    }
+
+    nav ul li a
+    {
+        font-size:16px;
+    }
+
+    nav ul
+    {
+        padding-inline-start:0px;
+        width:100%;
+        margin-block-end:0px;
+        left:0%;
+
+    }
+
+    nav ul li
+    {
+        height:6vh;
+        width:105px;
+    }
+
+
+
+    #img-li
+    {
+        width:140px;
+        height:6vh;
+    }
+
+    #img
+    {
+        width:140px;
+    }
+
+}
+
+@media screen and (min-width:1001px) and (max-width: 1337px)
+{
+
+    button
+    {
+        display:block;
+        margin-top: 30px;
+
+    }
+
+    nav ul li a
+    {
+        font-size:18px;
+    }
+
+    nav ul
+    {
+        padding-inline-start:0px;
+        width:100%;
+        margin-block-end:0px;
+        left:0%;
+
+    }
+
+    nav ul li
+    {
+        height:6vh;
+        width:130px;
+        padding-top: 5px;
+    }
+
+
+
+    #img-li
+    {
+        width:150px;
+        height:7vh;
+        margin-top: 10px;
+       
+    }
+
+    #img
+    {
+        width:150px;
+
+    }
+
+}
+
+
+
 </style>
