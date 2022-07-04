93d7948c54b040f7e7ea22d7a40ad15658f8efc8
iamrabie <kishmalakamran359@gmail.com>
2022-07-04 16:59:26 +0500

    content block overlay index file where overlay component is imported

---
+++ contentBlock-with-overlay/src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Overlay from '../components/overlay.svelte';
+    let heading3='THIS IS THE SAME HEADING';
+    let heading1='This is a title for the next page';
+    let text='This is a body of text';
+    let button='THIS IS A BUTTON';
+</script>
+
+
+<Overlay  {heading3} {heading1} {text} {button}/>
