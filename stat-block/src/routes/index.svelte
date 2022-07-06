4dd05feb6550d1da0d6edd8e20f821bc2053e83e
iamrabie <kishmalakamran359@gmail.com>
2022-07-06 18:00:31 +0500

    Index file where comp is imported

---
+++ stat-block/src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script>
+    import StatBlock from '../components/stat-block.svelte';
+
+    var Stats = [
+        {
+          'title' : 'Years of Excellence',
+          'EndingNumber' : 110,
+        },
+
+        {
+          'title' : 'Product SKUs',
+          'EndingNumber' : 150,
+        },
+
+        {
+          'title' : 'Team Members',
+          'EndingNumber' : 300,
+        } 
+
+        
+]
+
+console.log("Stats title" , Stats[0].title);
+console.log("Stats EndingNNumber" , Stats[0].EndingNumber);
+</script>
+
+<StatBlock {Stats} />
