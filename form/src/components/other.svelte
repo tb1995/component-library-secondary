e0f7a5a10aa09493210b8c45971cb96094b299a6
iamrabie <kishmalakamran359@gmail.com>
2022-07-21 14:17:09 +0500

    this is the component with dummy values

---
+++ form/src/components/other.svelte
@@ -0,0 +1,93 @@
+<script>
+
+</script>
+
+
+    <div class="flexCol">
+
+       <div class="flex">
+            <div class="fCW">
+                  fg
+            </div>
+            <div class="fCW2">
+                 513-900-1991
+            </div>
+       </div>
+
+        <div class="flex">
+             <div class="fCW">
+                 b
+             </div>
+             <div class="fCW2">
+                 info@luxorstonegroup.com
+             </div>
+        </div>
+
+        <div class="flex">
+             <div class="fCW">
+                 c
+             </div>
+             <div class="fCW2">
+                 2126 Stapleton Court,Cincinnati,Ohio 45240
+             </div>
+        </div>
+
+        <div class="flex">
+            <div class="fCW">
+                 d
+            </div>
+            <div class="fCW2">
+                <div class="flexColumn"></div>
+                      <div>
+                          Mon-Fri: 9am - 6pm
+                      </div>
+                      <div>
+                          Sat: 10am - 4pm
+                      </div>
+                </div>
+            </div>
+        </div>
+
+ 
+<style>
+
+    
+.flexCol
+    {
+        display:flex;
+        flex-direction: column;
+        margin-top: 24%;
+        margin-left: 5%;
+    }
+
+    .flexColumn
+    {
+        display: flex;
+        flex-direction: column;
+    }
+
+
+    .fCW
+    {
+        width:80px;
+        padding-bottom: 30px;
+        text-align: center;
+        color:white;
+    }
+
+    .fCW2
+    {
+        width:360px;
+        color:white;
+        font-weight: bold;
+    }
+
+    .flex
+    {
+        display: flex;
+    }
+
+   
+
+
+</style>
